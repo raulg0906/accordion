72f32a46e120356b61cc2ba9882dbb0da283cb30
raulg0906 <raulgonzalez1770@gmail.com>
2019-06-06 18:19:29 -0700

    finished accordion

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
+var accordions = document.getElementsByClassName("accordion")
 
-    $("#accordion").on("click", "h4", function() {
-      $(".text").removeClass("show")
-      $(this).find("+ .text").addClass("show")
-    })
-   })
+for (var i = 0; i < accordions.length; i++ ) {
+  accordions[i].onclick = function() {
+    var content = this.nextElementSibling;
+
+    if(content.style.maxHeight) {
+//accordion is open, we need to close it
+content.style.maxHeight = null;
+    }else {
+      //accordion is closed
+      content.style.maxHeight = content.scrollHeight + "px"
+    }
+  }
+}
